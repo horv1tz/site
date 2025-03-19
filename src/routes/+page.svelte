75f1562e739a05a4bb7f7da9894b99eca48b0c48
Leonid Dyachenko <horvitz@dvorfs.com>
2025-03-19 17:19:42 +0300

    feet(css)✨: Replace load css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -91,3 +91,34 @@
         </div>
     </div>
 {/if}
+
+<style>
+    .loading-screen {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgba(30, 30, 30, 0.8); /* Тёмный фон с прозрачностью */
+        z-index: 9999;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    .spinner {
+        border: 8px solid #f3f3f3;
+        border-top: 8px solid #5a1c36; /* Цвет спиннера */
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        animation: spin 2s linear infinite;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
